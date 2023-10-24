96eb8376e9b54584137c418822f029f15654be81
Ankur Agarwal <signup.ankur@gmail.com>
2023-10-24 11:41:58 +0530

    disabled log

--- apps/factory/src/server/auth.ts
+++ apps/factory/src/server/auth.ts
@@ -238,7 +238,7 @@ function getAuthOptions(): NextAuthOptions {
 }
 
 export const authOptions: NextAuthOptions = getAuthOptions();
-console.log("authOptions:", authOptions);
+// console.log("authOptions:", authOptions);
 
 /**
  * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
