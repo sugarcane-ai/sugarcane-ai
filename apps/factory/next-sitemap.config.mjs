586ce729137a041dd8f1dc068cad7dc7d492b861
Lovedeep <gurugobind.sh@gmail.com>
2023-12-05 02:55:08 +0530

    next-sitemap-2

--- apps/factory/next-sitemap.config.mjs
+++ apps/factory/next-sitemap.config.mjs
@@ -1,33 +1,57 @@
+// @ts-nocheck
 import { PackageVisibility, PrismaClient } from "@prisma/client";
+import * as Sentry from "@sentry/nextjs";
+
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-const prisma =   new PrismaClient({
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+const prisma = new PrismaClient({
   log:
-    process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
 });
 
 async function fetchPackageData() {
   try {
-    const packages = await prisma.promptPackage.findMany({
+    // throw new Error("Simulated error");
+    const packageData = await prisma.promptPackage.findMany({
       where: {
         visibility: PackageVisibility.PUBLIC,
       },
-      orderBy: {
-        updatedAt: "desc",
-      },
       include: {
         User: true,
+        templates: {
+          include: {
+            releaseVersion: true,
+            previewVersion: true,
+          },
+        },
       },
     });
-    console.log(`packages out -------------- ${JSON.stringify(packages)}`);
     
+    if (!packageData) {
+      return [];
+    }
+    const packages = packageData
+    console.log(packages)
+
     return packages.map((pkg) => {
       return {
         id: pkg.id,
-        authorName: pkg.User.name,
+        authorName: pkg.User.username,
+        name: pkg.name,
+        templateName: pkg.templates,
       };
     });
   } catch (error) {
     console.error("Failed to fetch package data:", error);
+    if (SENTRY_DSN) {
+      Sentry.init({
+        dsn: SENTRY_DSN, // Initialize Sentry with your DSN
+      });
+      Sentry.captureException(error); // Send the exception to Sentry
+    }
     return [];
   }
 }
@@ -37,19 +61,41 @@ export default {
   siteUrl: process.env.NEXTAUTH_URL,
   generateRobotsTxt: true,
   changefreq: "daily",
-  exclude: ['/dashboard/prompts',],
+  exclude: ["/dashboard/prompts"],
   additionalPaths: async () => {
+    const result = [];
     const packageData = await fetchPackageData();
-    console.log(packageData);
-  
-    return packageData.map((data) => {
+
+    packageData.forEach((data) => {
       const loc = `/marketplace/packages/${data.id}`;
-      const authorName = data.authorName;
-  
-      return {
+      result.push({
         loc: loc,
-        authorName,
-      };
+        authorName: data.authorName,
+        lastmod: new Date().toISOString(),
+      });
+
+      data.templateName.forEach((test) => {
+        if(test.releaseVersion){
+          const sugarcubes = `/${data.authorName}/${data.name}/${test.name}/release`;
+          result.push({
+            loc: sugarcubes,
+            authorName: data.authorName,
+            lastmod: new Date().toISOString(),
+          });
+        }
+        if(test.previewVersion){
+          const sugarcubes = `/${data.authorName}/${data.name}/${test.name}/preview`;
+          result.push({
+            loc: sugarcubes,
+            authorName: data.authorName,
+            lastmod: new Date().toISOString(),
+          });
+
+        }
+      });
     });
+
+    return result;
   },
 };
+
