4f73d8274ecc3109c41b52675f7c7b3c019d2c87
Lovedeep <91473234+LovedeepSingh19@users.noreply.github.com>
2023-12-10 19:32:13 +0530

    Update hero.tsx

--- apps/factory/src/components/landing/hero.tsx
+++ apps/factory/src/components/landing/hero.tsx
@@ -38,7 +38,7 @@ const Hero = () => {
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
               <Link
                 href={
-                  sessionData?.user ? "/dashboard/prompts" : "/api/auth/signin"
+                  sessionData!.user ? "/dashboard/prompts" : "/api/auth/signin"
                 }
                 className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               >
