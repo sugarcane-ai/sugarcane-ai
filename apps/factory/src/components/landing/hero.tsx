9bdc1a3259fd56cacaa18a5b4f9e2b9163d67c99
Lovedeep <gurugobind.sh@gmail.com>
2023-12-11 17:57:22 +0530

    test4

--- apps/factory/src/components/landing/hero.tsx
+++ apps/factory/src/components/landing/hero.tsx
@@ -38,7 +38,14 @@ const Hero = () => {
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
               <Link
                 href={
-                  sessionData ? "/dashboard/prompts" : "/api/auth/signin"
+                  sessionData
+                    ? {
+                        pathname: "/dashboard/prompts",
+                      }
+                    : {
+                        pathname: "/api/auth/signin",
+                      }
+                  // "/dashboard/prompts" : "/api/auth/signin"
                 }
                 className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               >
