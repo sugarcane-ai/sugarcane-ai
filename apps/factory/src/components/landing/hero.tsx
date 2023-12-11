fad8b2a2dcf4203f6a295baeb9d3efa6e4090489
Lovedeep <gurugobind.sh@gmail.com>
2023-12-11 18:10:57 +0530

    test5

--- apps/factory/src/components/landing/hero.tsx
+++ apps/factory/src/components/landing/hero.tsx
@@ -36,24 +36,37 @@ const Hero = () => {
             </div>
           ) : (
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-              <Link
-                href={
-                  sessionData
-                    ? {
-                        pathname: "/dashboard/prompts",
-                      }
-                    : {
-                        pathname: "/api/auth/signin",
-                      }
-                  // "/dashboard/prompts" : "/api/auth/signin"
-                }
-                className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              >
-                <h3 className="text-2xl font-bold">Sugar Factory →</h3>
-                <div className="text-lg">
-                  Build, Version, Train and Ship Prompt Packages over APIs.
-                </div>
-              </Link>
+              {sessionData ? (
+                <Link
+                  href={
+                    {
+                      pathname: "/dashboard/prompts",
+                    }
+                    // "/dashboard/prompts" : "/api/auth/signin"
+                  }
+                  className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+                >
+                  <h3 className="text-2xl font-bold">Sugar Factory →</h3>
+                  <div className="text-lg">
+                    Build, Version, Train and Ship Prompt Packages over APIs.
+                  </div>
+                </Link>
+              ) : (
+                <Link
+                  href={
+                    {
+                      pathname: "/api/auth/signin",
+                    }
+                    // "/dashboard/prompts" : "/api/auth/signin"
+                  }
+                  className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
+                >
+                  <h3 className="text-2xl font-bold">Sugar Factory →</h3>
+                  <div className="text-lg">
+                    Build, Version, Train and Ship Prompt Packages over APIs.
+                  </div>
+                </Link>
+              )}
               <Link
                 className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
                 href="/marketplace/packages"
