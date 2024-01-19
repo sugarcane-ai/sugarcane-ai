ea18505aa392bef78349f41ffaf26e54d9381552
Rahul917-Kumar <rahul66.ugcs20@iiitranchi.ac.in>
2024-01-19 19:38:24 +0530

    improve UI of Sugar cube

--- apps/factory/src/services/vendors/openai_vendors.ts
+++ apps/factory/src/services/vendors/openai_vendors.ts
@@ -79,7 +79,7 @@ class OpenAIVendor extends BaseVendor {
     const res = await this.openai.images.generate({
       prompt: prompt,
       n: 1,
-      size: "256x256",
+      size: "512x512",
     });
     let response: DalleSchemaType = {
       created: "",
